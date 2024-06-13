edb4be2d00c356e50720a1db92b713930c897158
00CHI <suinmoon83@gmail.com>
2024-06-13 10:35:28 +0900

    20240613Oz_01

--- Assets/02_Scritps/05_Witch01Dialogue.cs
+++ Assets/02_Scritps/05_Witch01Dialogue.cs
@@ -5,28 +5,42 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Rendering.UI;
 using UnityEngine.UIElements;
+using System.IO.Pipes;
 
 public class Witch01Dialogue : MonoBehaviour
 {
+    Dictionary <int, string[]> talkData;
+
     [SerializeField]
     private TextMeshProUGUI witchTalk;
 
-   public PanelTextSettings WitchDialogue;
+    public PanelTextSettings WitchDialogue;
 
+
+    private void Awake()
+    {
+        talkData = new Dictionary <int, string[]>();
+        GenerateData();
+    }
     private void Start()
     {
         
     }
     void Update()
     {
-        if(witchTalk != null)
-        WitchText_01();
+
     }
 
-    void WitchText_01()
+    void GenerateData()
     {
-        witchTalk.text = "안녕하세요. 저는 이 숲과 난쟁이들을 지키는 북쪽 마녀예요.\n난쟁이를 괴롭히던 동쪽 마녀를 없애주셔서 감사해요.";
+        talkData.Add(500, new string[] { "안녕하세요. 저는 이 숲과 난쟁이들을 지키는 북쪽 마녀예요.\n난쟁이를 괴롭히던 동쪽 마녀를 없애주셔서 감사해요." });
+        talkData.Add(100, new string[] { "언니 사실 범인이죠?, 거짓말치지 마요.  " });
     }
 
-
+    public string GetTalk(int id, int talkIndex)
+    {
+        return talkData[id][talkIndex];
+    }
 }
+
+
---
+++ Assets/02_Scritps/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.UI;
+using TMPro;
+using UnityEngine.UIElements;
+using System.IO.Pipes;
+
+
+public class GameManager : MonoBehaviour
+{
+    public ObjManager objManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
