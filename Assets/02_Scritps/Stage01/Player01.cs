1d3a52c9819c2449c43a6bb5fd1d790a69505684
00CHI <suinmoon83@gmail.com>
2024-11-27 05:53:40 +0900

    241126
    -1층 맵배치
    -튜토리얼 맵배치
    - 7층 서재, 감옥 맵배치
    
    완성

--- Assets/02_Scritps/Stage01/Player01.cs
+++ Assets/02_Scritps/Stage01/Player01.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Player01 : MonoBehaviour
 {
@@ -18,8 +17,7 @@ public class Player01 : MonoBehaviour
 
     //Variable
     private float h = 0;
-
-    private bool isPortal01;
+    private float j = 0;
 
     // Start is called before the first frame update
     void Awake()
@@ -32,12 +30,17 @@ public class Player01 : MonoBehaviour
     void Start()
     {
         h = Input.GetAxisRaw("Horizontal");
+        j = Input.GetAxisRaw("Jump");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Derection flip Sprite //Run Anim
+        if (Input.GetButtonUp("Jump"))
+        {
+            anim.SetBool("isJumpdown", true);
+        }
 
         //Run Animation
         if (rigid.velocity.normalized.x == 0)
@@ -53,6 +56,36 @@ public class Player01 : MonoBehaviour
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
+        //Jump /jump limited == && !anim.GetBool("isJump")
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJump") && !anim.GetBool("isJumpdown"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isRun", false);
+
+            //Jump Animation
+            anim.SetBool("isJump", true);
+        }
+
+        //Jump Down
+        if (rigid.velocity.y < -0.01f)
+        {
+            //false anim
+            anim.SetBool("isJump", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isIdle", false);
+
+            // true anim
+            anim.SetBool("isJumpdown", true);
+
+            //Gravity Ctrl
+            rigid.AddForce(Vector2.down * jumpDown * Time.deltaTime);
+        }
+        // Jump down -> Idle 
+        else
+        {
+            anim.SetBool("isJumpdown", false);
+            anim.SetBool("isIdle", true);
+        }
 
         //Derection flip Sprite //Run Anim
         if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -62,11 +95,6 @@ public class Player01 : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             spriteRenderer.flipX = false;
-        }
-
-        if(Input.GetKeyDown(KeyCode.E) && isPortal01)
-        {
-            SceneManager.LoadScene("Stage2");
         }
     }
 
@@ -88,13 +116,41 @@ public class Player01 : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Portal01"))
+        //Box Tag
+        if (collision.gameObject.CompareTag("Box"))
         {
-            isPortal01 = true;
+            float yGap = transform.position.y - collision.gameObject.transform.position.y;
+            //Debug.LogError("GAP: " + yGap);
+
+            if (yGap < 1)
+            {
+                anim.SetBool("isPull", true);
+                anim.SetBool("isRun", false);
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isJump", false);
+            }
+            else
+            {
+                anim.SetBool("isJump", false);
+                anim.SetBool("isPull", false);
+                anim.SetBool("isRun", true);
+                anim.SetBool("isIdle", true);
+            }
         }
+    }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //Box Tag
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            anim.SetBool("isJump", false);
+            anim.SetBool("isPull", false);
+            anim.SetBool("isRun", true);
+            anim.SetBool("isIdle", true);
 
+        }
     }
 }
